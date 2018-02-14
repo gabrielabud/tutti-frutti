930234094a7542c6890fc13057483f9f053af237
tmerrr <trmoir9@gmail.com>
2018-02-14 11:57:02 +0000

    first scrape running, but need to manipulate data

--- app.js
+++ app.js
@@ -37,10 +37,30 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const options = {
+  uri: 'http://www.fruitfortheoffice.co.uk/tailormade-24/fruit-bowl-44/apples-pears-29/',
+  transform: function(body) {
+    return cheerio.load(body);
+  }
+}
+
+rp(options)
+    .then((res) => {
+        console.log('LOGGING THE DOLLAR')
+        console.log(res)
+    })
+    .catch((err) => {
+        console.log('LOGGING THE ERR')
+        console.log(err)
+    })
 
 module.exports = app;
