510f040cf0d833ba73a3e676e2aca549ecda3474
tmerrr <trmoir9@gmail.com>
2018-02-19 09:58:44 +0000

    refactors the POST route for saving fruits and extracts the scrape into a controller

--- controllers/fruits.js
+++ controllers/fruits.js
@@ -1,17 +1,41 @@
+const rp = require('request-promise');
+const cheerio = require('cheerio');
 const Fruit = require('../models/fruits');
 
 const saveNewFruit = (name, price) => {
   Fruit.find({ name: name }, (err, searchResults) => {
     if (!searchResults.length) {
-        var fruit = new Fruit({
-            name  : name,
-            price : price
-        })
-        fruit.save((err) => {
-            if (err) throw err;
-        });  
+      var fruit = new Fruit({
+        name  : name,
+        price : price
+      })
+      fruit.save((err) => {
+        if (err) throw err;
+      });  
     }
   }) 
 }
 
-module.exports = saveNewFruit;
+const scrape = (uri) => {
+  let options = {
+    uri: uri,
+    transform: (body) => {
+      return cheerio.load(body);
+    }
+  }
+  rp(options)
+    .then(($) => {
+      var productNames  = $('.tailor-made-product-name').toArray();
+      var productPrices = $('.tailor-made-product-price-box').toArray();
+      productNames.forEach((item, i) => {
+        let itemName  = item.children[0].data.trim();
+        let itemPrice = productPrices[i].children[0].data.trim().replace(/£/, '');
+        saveNewFruit(itemName, itemPrice);
+      })
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}
+
+module.exports = scrape;
--- routes/fruits.js
+++ routes/fruits.js
@@ -5,40 +5,17 @@ const rp = require('request-promise');
 const cheerio = require('cheerio');
 
 const Fruit = require('../models/fruits.js');
-const saveNewFruit = require('../controllers/fruits');
+const scrape = require('../controllers/fruits');
 
 
 router.post('/new', (req, res) => {
-    let fruits = [];
-    urisToScrape = [
+    let urisToScrape = [
         "http://www.fruitfortheoffice.co.uk/tailormade-24/fruit-bowl-44/apples-pears-29/",
         "http://www.fruitfortheoffice.co.uk/tailormade-24/fruit-bowl-44/bananas-30/",
         "http://www.fruitfortheoffice.co.uk/tailormade-24/fruit-bowl-44/citrus-fruits-40/"
     ]
-    urisToScrape.forEach((uri, index) => {
-        let options = {
-            uri: uri,
-            transform: (body) => {
-                return cheerio.load(body);
-            }
-        }
-        rp(options)
-            .then(($) => {
-                var productNames  = $('.tailor-made-product-name').toArray();
-                var productPrices = $('.tailor-made-product-price-box').toArray();
-                productNames.forEach((item, i) => {
-                    let itemName  = item.children[0].data.trim();
-                    let itemPrice = productPrices[i].children[0].data.trim().replace(/£/, '');
-                    saveNewFruit(itemName, itemPrice);
-                })
-                if (index === urisToScrape.length - 1) {
-                    res.status(200).send('Save successful');
-                }
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-    })
+    urisToScrape.forEach((uri) => scrape(uri));
+    res.status(200).send('Save successful')
 })
 
 router.get('/', (req, res) => {
