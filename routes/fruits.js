a69ccce02327ee4967d49a7e9e405190fb535791
Tom Moir <tommoir@jigsaw.xyz>
2018-02-22 15:12:00 +0000

    extracts pages array into a Data directory

--- routes/fruits.js
+++ routes/fruits.js
@@ -5,23 +5,9 @@ const scrapePage      = require('../controllers/scrapers');
 const Fruit           = require('../models/fruits');
 const Category        = require('../models/categories');
 const CategoryMethods = require('../controllers/categories');
+const productPages    = require('../data/pages');
 
 router.post('/new', (req, res) => {
-  let productPages = [
-    {
-      category : "Apples and Pears",
-      uri      : "http://www.fruitfortheoffice.co.uk/tailormade-24/fruit-bowl-44/apples-pears-29/"
-    },
-    {
-      category : "Bananas",
-      uri      : "http://www.fruitfortheoffice.co.uk/tailormade-24/fruit-bowl-44/bananas-30/"
-    },
-    {
-      category : "Citrus Fruits",
-      uri      : "http://www.fruitfortheoffice.co.uk/tailormade-24/fruit-bowl-44/citrus-fruits-40/"
-    }
-  ]
-
   Promise.map(productPages, async page => {
     await CategoryMethods.findOrCreateCategory(page.category);
     let categoryId = await CategoryMethods.getCategoryId(page.category);
